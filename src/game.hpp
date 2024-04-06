543b66dec69ad795e928c79c6b85061ca1d80c9c
Erthium <ertyum35@gmail.com>
2024-04-06 21:32:40 +0300

    feat: update core functions according to the master

--- src/game.hpp
+++ src/game.hpp
@@ -2,11 +2,18 @@
 #include <SDL_image.h>
 #include <SDL_timer.h>
 #include "agent.hpp"
+#include "master.hpp"
 
 using namespace std;
 
-void draw_grid(SDL_Renderer* renderer, Agent* agents, int WIDTH, int HEIGHT);
+void draw_agents(SDL_Renderer* renderer, Agent* agents, Master master);
 
-void update_agents(Agent* agents, int WIDTH, int HEIGHT);
+Agent* spawn_agents(Master master);
 
-Agent* spawn_agents(int agent_count, int WIDTH, int HEIGHT);
+bool is_in_visual(Agent agent, Agent other, Master master);
+
+bool is_in_protected_range(Agent agent, Agent other, Master master);
+
+void update_agents(Agent* agents, Master master);
+
+void move_agents(Agent* agents, Master master);
