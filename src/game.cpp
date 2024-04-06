9ceb6cd34af58f27857e38f2073bfdd6b9d1c238
Erthium <ertyum35@gmail.com>
2024-04-06 21:35:07 +0300

    feat: implement other basic functions

--- src/game.cpp
+++ src/game.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <time.h>
+#include <vector>
 #include "game.hpp"
 
 
@@ -33,3 +34,41 @@ Agent* spawn_agents(Master master) {
   return agents;
 }
 
+
+bool is_in_visual(Agent agent, Agent other, Master master) {
+  int dx = agent.x - other.x;
+  int dy = agent.y - other.y;
+  return sqrt(dx * dx + dy * dy) < master.VISUAL_RANGE;
+}
+
+
+bool is_in_protected_range(Agent agent, Agent other, Master master) {
+  int dx = agent.x - other.x;
+  int dy = agent.y - other.y;
+  return sqrt(dx * dx + dy * dy) < master.PROTECTED_RANGE;
+}
+
+
+void update_agents(Agent* agents, Master master) {
+  return;
+}
+
+
+void move_agents(Agent* agents, Master master) {
+  // move agent according to its speed
+  for (int i = 0; i < master.AGENT_COUNT; i++)  {
+    agents[i].x += agents[i].speed_x;
+    agents[i].y += agents[i].speed_y;
+    // min max speed clamping
+    // min max speed represents the minimum and maximum velocity an agent can have, direction included
+    float total_speed = sqrt(agents[i].speed_x * agents[i].speed_x + agents[i].speed_y * agents[i].speed_y);
+    if (total_speed > master.MAX_SPEED) {
+      agents[i].speed_x = agents[i].speed_x / total_speed * master.MAX_SPEED;
+      agents[i].speed_y = agents[i].speed_y / total_speed * master.MAX_SPEED;
+    }
+    if (total_speed < master.MIN_SPEED) {
+      agents[i].speed_x = agents[i].speed_x / total_speed * master.MIN_SPEED;
+      agents[i].speed_y = agents[i].speed_y / total_speed * master.MIN_SPEED;
+    }
+  }
+}
