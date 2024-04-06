a1eb4b98d2e6cb4ad0dca85d431d7b399ce467b7
Erthium <ertyum35@gmail.com>
2024-04-07 02:21:50 +0300

    chore: remove all macros and use master struct directly

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,23 @@
 
 using namespace std;
 
-#define FPS 60
-#define WIDTH 1800
-#define HEIGHT 900
-#define MARGIN 10
-
-#define AGENT_COUNT 40
-
-// agents configuration
-#define VISUAL_RANGE 140
-#define PROTECTED_RANGE 20
-#define MAX_SPEED 3
-#define MIN_SPEED 2
-#define TURN_FACTOR 0.2
-#define MATCH_FACTOR 0.005
-#define AVOID_FACTOR 0.009
-#define CENTER_FACTOR 0.0005
-
 int main(int argc, char* argv[]) {
+
+    Master master;
+    master.WIDTH = 1800;
+    master.HEIGHT = 900;
+    master.AGENT_COUNT = 100;
+    master.FPS = 60;
+    master.MARGIN = 40;
+    master.VISUAL_RANGE = 140;
+    master.PROTECTED_RANGE = 10;
+    master.MAX_SPEED = 5;
+    master.MIN_SPEED = 2;
+    master.TURN_FACTOR = 0.2;
+    master.MATCH_FACTOR = 0.005;
+    master.AVOID_FACTOR = 0.004;
+    master.CENTER_FACTOR = 0.0005;
+    master.DEBUG = false;
 
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
     cout << "Error initializing SDL: " << SDL_GetError() << endl;
@@ -32,28 +31,12 @@ int main(int argc, char* argv[]) {
     "Boids Simulation",
     SDL_WINDOWPOS_CENTERED,
     SDL_WINDOWPOS_CENTERED,
-    WIDTH,
-    HEIGHT,
+    master.WIDTH,
+    master.HEIGHT,
     0
   );
   SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
   cout << "Game initialized." << endl;
-
-  Master master = {
-    WIDTH,
-    HEIGHT,
-    AGENT_COUNT,
-    FPS,
-    MARGIN,
-    TURN_FACTOR,
-    MATCH_FACTOR,
-    AVOID_FACTOR,
-    CENTER_FACTOR,
-    VISUAL_RANGE,
-    PROTECTED_RANGE,
-    MAX_SPEED,
-    MIN_SPEED
-  };
 
   Agent* agents = spawn_agents(master);
 
@@ -112,6 +95,5 @@ int main(int argc, char* argv[]) {
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
-
   return 0;
 }
