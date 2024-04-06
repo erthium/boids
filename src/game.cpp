ce1af051619a0984802c00964436ca61eea3c962
Erthium <ertyum35@gmail.com>
2024-04-06 21:34:18 +0300

    feat: implement draw_agents function without having the agents facing their direction

--- src/game.cpp
+++ src/game.cpp
@@ -2,13 +2,21 @@
 #include <time.h>
 #include "game.hpp"
 
-void draw_grid(SDL_Renderer* renderer, Agent* grid, size_t size) {
-  return;
-}
 
+void draw_agents(SDL_Renderer* renderer, Agent* agents, Master master) {
+  // draw a triangle for each agent according to where they are facing, long part of the triangle will face there
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
+  
+  // render every agent
+  for (int i = 0; i < master.AGENT_COUNT; i++) {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDrawLine(renderer, agents[i].x, agents[i].y, agents[i].x + 10, agents[i].y + 10);
+    SDL_RenderDrawLine(renderer, agents[i].x, agents[i].y, agents[i].x - 10, agents[i].y + 10);
+    SDL_RenderDrawLine(renderer, agents[i].x + 10, agents[i].y + 10, agents[i].x - 10, agents[i].y + 10);
+  }
 
-void update_grid(Agent* grid, size_t size) {
-  return;
+  SDL_RenderPresent(renderer);
 }
 
 
